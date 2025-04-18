8a23b04a696da3e02323769f5e89f07852585bc6
vdragon1225 <23laidan@gmail.com>
2025-04-18 11:58:03 -0500

    Update Inventory System
    Changed slot_gui to button to move items and created itemStackGui. Also, started working on hotbar but need to finish itemStackGui first

---
+++ scripts/hotbar_slot.gd
@@ -0,0 +1,9 @@
+extends Panel
+
+@onready var background_sprite: Sprite2D = $background
+#@onready var item_stack_gui: ItemStackGui = $CenterContainer/Panel
+
+#func update_to_slot(slot: InventorySlot) -> void:
+	#if !slot.item:
+		#item_stack_gui.visible = false
+		#
