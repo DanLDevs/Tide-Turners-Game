44846ea07011e8cd033341252920df67cc72af5e
vdragon1225 <23laidan@gmail.com>
2025-04-20 21:21:55 -0500

    Added Stardew Valley music

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+func _ready():
+	var music_player = $MusicPlayer
+	var stream = music_player.stream
+	if stream is AudioStream:
+		stream.loop = true
+	music_player.play()
+
 
 func _on_inventory_gui_closed() -> void:
 	get_tree().paused = false
